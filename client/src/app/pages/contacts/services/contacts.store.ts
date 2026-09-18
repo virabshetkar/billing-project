511c9bafebb857c6ca4ce7c08560e29d12a67b1b
Virab Shetkar <virabarunshetkar@gmail.com>
2026-09-18 12:06:48 +0530

    Fixed update and create bug

--- client/src/app/pages/contacts/services/contacts.store.ts
+++ client/src/app/pages/contacts/services/contacts.store.ts
@@ -1,7 +1,7 @@
 import { httpResource } from '@angular/common/http';
 import { computed, inject } from '@angular/core';
 import { signalStore, withState, withProps, withMethods, patchState } from '@ngrx/signals';
-import { Observable, of, tap } from 'rxjs';
+import { map, Observable, of, switchMap, tap } from 'rxjs';
 import { Contact } from '../models/contact';
 import { ContactsApi, CreateContactDto, UpdateContactDto } from './contacts.api';
 
@@ -14,6 +14,8 @@ export interface ContactsStoreState {
   isLoading: boolean;
   lastUpdatedContactId: string | null;
 }
+
+type InsertionResult = { type: 'insert'; index: number } | { type: 'already-loaded' };
 
 export const ContactsStore = signalStore(
   withState<ContactsStoreState>({
@@ -30,90 +32,176 @@ export const ContactsStore = signalStore(
     selectedContact: httpResource<Contact>(() =>
       selectedContactId() ? `/api/contacts/${selectedContactId()}` : undefined,
     ),
+
     lastUpdatedContact: computed(() => contactList().find((c) => c.id === lastUpdatedContactId())),
   })),
 
-  withMethods((store, contactsApi = inject(ContactsApi)) => ({
-    selectContact(id: string) {
-      patchState(store, (state) => ({ ...state, selectedContactId: id }));
-    },
+  withMethods((store, contactsApi = inject(ContactsApi)) => {
+    const compareContacts = (a: Contact, b: Contact) => a.name.localeCompare(b.name);
 
-    clearSelectedContact() {
-      patchState(store, (state) => ({ ...state, selectedContactId: null }));
-    },
+    const findInsertionIndex = (contacts: Contact[], contact: Contact, left = 0): number => {
+      let right = contacts.length;
 
-    create(contact: CreateContactDto) {
-      return contactsApi.createContact(contact).pipe(
-        tap((contact) => {
-          patchState(store, (state) => ({
-            ...state,
-            contactList: [contact, ...state.contactList].sort((a, b) =>
-              a.name.localeCompare(b.name),
-            ),
-          }));
-        }),
-      );
-    },
+      while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
 
-    update(id: string, contact: UpdateContactDto) {
-      return contactsApi.updateContact(id, contact).pipe(
-        tap((c) => {
-          const newContactListState = store.contactList();
+        if (compareContacts(contacts[mid], contact) < 0) {
+          left = mid + 1;
+        } else {
+          right = mid;
+        }
+      }
 
-          const index = newContactListState.findIndex((con) => con.id === c.id);
-          if (index !== -1) newContactListState.splice(index, 1);
+      return left;
+    };
 
-          patchState(store, (state) => ({
-            ...state,
-            contactList: [c, ...newContactListState].sort((a, b) => a.name.localeCompare(b.name)),
-            lastUpdatedContactId: c.id,
-          }));
+    const findInsertionPoint = (contact: Contact, left = 0): Observable<InsertionResult> => {
+      const contacts = store.contactList();
 
-          store.selectedContact?.reload();
-        }),
-      );
-    },
+      // The server has already returned this contact.
+      if (contacts.some((c) => c.id === contact.id)) {
+        return of({ type: 'already-loaded' });
+      }
 
-    delete(id: string) {
-      return contactsApi.deleteContact(id).pipe(
-        tap((c) => {
-          const newContactList = store.contactList();
-          const index = newContactList.findIndex((con) => con.id === c.id);
-          if (index !== -1) newContactList.splice(index, 1);
+      const index = findInsertionIndex(contacts, contact, left);
 
-          patchState(store, (state) => ({
-            ...state,
-            selectedContactId: undefined,
-            contactList: newContactList,
-          }));
-        }),
-      );
-    },
+      // We have a loaded contact after the new contact.
+      if (index < contacts.length) {
+        return of({
+          type: 'insert',
+          index,
+        });
+      }
 
-    loadNext(): Observable<Contact[] | null> {
-      const skip = store.contactList().length;
+      // We've reached the end of the entire dataset.
+      if (!store.hasNext()) {
+        return of({
+          type: 'insert',
+          index,
+        });
+      }
 
-      if (!store.hasNext() || store.isLoading()) return of(null);
+      const nextLeft = contacts.length;
 
-      patchState(store, (state) => ({ ...state, isLoading: true }));
+      return loadNext().pipe(switchMap(() => findInsertionPoint(contact, nextLeft)));
+    };
 
-      return contactsApi.getContacts(store.limit(), skip).pipe(
-        tap((c) => {
-          patchState(store, (state) => ({
-            ...state,
-            contactList: [...state.contactList, ...c],
-            isLoading: false,
-            hasNext: store.limit() === c.length,
-          }));
-        }),
-      );
-    },
-
-    reset() {
+    const insertAt = (contact: Contact, index: number) => {
       patchState(store, (state) => ({
         ...state,
-        contactList: [],
+        contactList: [
+          ...state.contactList.slice(0, index),
+          contact,
+          ...state.contactList.slice(index),
+        ],
       }));
-    },
-  })),
+    };
+
+    const loadNext = (): Observable<Contact[] | null> => {
+      const skip = store.contactList().length;
+
+      if (!store.hasNext() || store.isLoading()) {
+        return of(null);
+      }
+
+      patchState(store, (state) => ({
+        ...state,
+        isLoading: true,
+      }));
+
+      return contactsApi.getContacts(store.limit(), skip).pipe(
+        tap((contacts) => {
+          patchState(store, (state) => ({
+            ...state,
+            contactList: [...state.contactList, ...contacts],
+            isLoading: false,
+            hasNext: store.limit() === contacts.length,
+          }));
+        }),
+      );
+    };
+
+    return {
+      selectContact(id: string) {
+        patchState(store, (state) => ({
+          ...state,
+          selectedContactId: id,
+        }));
+      },
+
+      clearSelectedContact() {
+        patchState(store, (state) => ({
+          ...state,
+          selectedContactId: null,
+        }));
+      },
+
+      create(contact: CreateContactDto): Observable<Contact> {
+        return contactsApi.createContact(contact).pipe(
+          switchMap((createdContact) =>
+            findInsertionPoint(createdContact).pipe(
+              tap((result) => {
+                if (result.type === 'insert') {
+                  insertAt(createdContact, result.index);
+                }
+                patchState(store, (state) => ({
+                  ...state,
+                  lastUpdatedContactId: createdContact.id,
+                }));
+              }),
+              map(() => createdContact),
+            ),
+          ),
+        );
+      },
+
+      update(id: string, contact: UpdateContactDto): Observable<Contact> {
+        return contactsApi.updateContact(id, contact).pipe(
+          switchMap((updatedContact) => {
+            patchState(store, (state) => ({
+              ...state,
+              contactList: state.contactList.filter((c) => c.id !== updatedContact.id),
+            }));
+
+            return findInsertionPoint(updatedContact).pipe(
+              tap((result) => {
+                if (result.type === 'insert') {
+                  insertAt(updatedContact, result.index);
+                }
+
+                patchState(store, (state) => ({
+                  ...state,
+                  lastUpdatedContactId: updatedContact.id,
+                }));
+
+                store.selectedContact?.reload();
+              }),
+              map(() => updatedContact),
+            );
+          }),
+        );
+      },
+
+      delete(id: string) {
+        return contactsApi.deleteContact(id).pipe(
+          tap((c) => {
+            patchState(store, (state) => ({
+              ...state,
+              selectedContactId: null,
+              contactList: state.contactList.filter((con) => con.id !== c.id),
+            }));
+          }),
+        );
+      },
+
+      loadNext,
+
+      reset() {
+        patchState(store, (state) => ({
+          ...state,
+          contactList: [],
+        }));
+      },
+    };
+  }),
 );
